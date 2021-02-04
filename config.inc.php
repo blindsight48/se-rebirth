de1b57e3d93e22ccb9879785ca323cccf57f62f0
blindsight48 <78524879+blindsight48@users.noreply.github.com>
2021-02-04 01:01:24 -0500

    Update config.inc.php
    added commented instructions and am testing this github function

--- config.inc.php
+++ config.inc.php
@@ -5,3 +5,6 @@ define("USER", "root");
 define("PASS", "password");
 define("DB", "v-0.1alpha");
 define("SERVER", "test");
+
+// Clone this template file into a new directory, up one level called '/includes'. 
+// Edit the constants to fit your server. 
